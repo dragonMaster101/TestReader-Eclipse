662dfd401d4dca93c6b7088ce6917324f37e42bf
dragonMaster101 <a.humeidh@gmail.com>
2015-11-20 02:04:15 +0800

    initial commit

---
+++ src/com/eju/testReader/MainActivity.java
@@ -0,0 +1,18 @@
+package com.eju.testReader;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+
+
+public class MainActivity extends DroidGap
+ {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+}
